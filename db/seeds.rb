422e99d267b2cb431f437e8251b7ba8e80867efd
Adam Heckathorn <ajheckathorn01@gmail.com>
2021-05-04 20:20:22 -0500

    product 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.create({name: "protein", price 20, description: "whey protein powder", image_url: ""})
