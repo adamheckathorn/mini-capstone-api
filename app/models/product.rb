2884bb879ef59be7c5d8562cace9cf7e8abfbf05
Adam Heckathorn <ajheckathorn01@gmail.com>
2021-05-04 20:16:08 -0500

    edits

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,2 @@
 class Product < ApplicationRecord
-  def initialize(options)
-    @name = name[options]
-    @price = price[options].to_i
-    @image_url = image_url[options]
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-def initialize(options)
-  @name = name[options]
-  @price = price[options].to_i
-  @image_url = image_url[options]
+
 end
