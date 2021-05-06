fc9ff4204a84af4823494364dcc49171442e8b9a
Adam Heckathorn <ajheckathorn01@gmail.com>
2021-05-06 18:27:59 -0500

    index and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,26 @@
 class ProductsController < ApplicationController
-  def one_product
-    product = Product.first
+  def one_product_method
+    one_product = Product.find_by(id: params["id"])
+    render json: one_product.as_json
+  end
+
+  def all_products_method
+    products = Product.all
     render json: product.as_json
   end
 
-  def all_product_method
-    products = Product.all
+  def url_segment_params
+    product = Product.something
+    render json: product.as_json
+  end
+
+  def index
+    product = Product.all
+    render json: product.as_json
+  end
+
+  def show
+    product = Product.find_by(id: params[:id])
     render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get "/url_segment_params", controller: "params_examples", action: "url_segment_params"
 end
